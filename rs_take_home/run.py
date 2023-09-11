245561544a66c99933077aa881bee34b9c547906
nicklamiller <nicklamiller@gmail.com>
2023-09-10 17:32:45 -0700

    change inputs so consistent with test description

--- rs_take_home/run.py
+++ rs_take_home/run.py
@@ -4,6 +4,7 @@ from typing import List
 from pyspark.sql import SparkSession
 
 from rs_take_home.association_counter import AssociationCounter
+from rs_take_home.data_models import DiseaseHierarchy, GeneDiseaseAssociations
 
 spark = (
     SparkSession
@@ -25,11 +26,31 @@ _default_gene_disease_queries = [
     ('ENSG00000213689', 'MONDO:0018827'),
 ]
 
+_default_gene_disease_associations = (
+    GeneDiseaseAssociations
+    .from_filepath(_filepaths_config['gene_disease_associations'], spark)
+)
+
+
+_default_disease_hierarchy = (
+    DiseaseHierarchy
+    .from_filepath(_filepaths_config['disease_hierarchy'], spark)
+)
+
 
 def get_association_counts(
+    gene_disease_associations: GeneDiseaseAssociations = None,
+    disease_hierarchy: DiseaseHierarchy = None,
     queries: List[tuple] = _default_gene_disease_queries,
-    config: dict = _filepaths_config,
     spark: SparkSession = spark,
 ):
-    association_counter = AssociationCounter.from_config(config, spark)
+    if not gene_disease_associations:
+        gene_disease_associations = _default_gene_disease_associations
+    if not disease_hierarchy:
+        disease_hierarchy = _default_disease_hierarchy
+    association_counter = AssociationCounter(
+        disease_hierarchy=disease_hierarchy,
+        gene_disease_associations=gene_disease_associations,
+        spark=spark,
+    )
     return association_counter.count_all_queries(queries)
