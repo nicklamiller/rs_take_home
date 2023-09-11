4f487d4a91afc61f0907ff77d8b49d8fda3e51ee
nicklamiller <nicklamiller@gmail.com>
2023-09-10 17:44:32 -0700

    change inputs to be dataframes for get_association_counts

--- rs_take_home/run.py
+++ rs_take_home/run.py
@@ -1,10 +1,11 @@
 import json
 from typing import List
 
-from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame, SparkSession
 
 from rs_take_home.association_counter import AssociationCounter
 from rs_take_home.data_models import DiseaseHierarchy, GeneDiseaseAssociations
+from rs_take_home.utils import spark_read_csv
 
 spark = (
     SparkSession
@@ -19,35 +20,33 @@ with open('config/filepaths.json') as json_file:
     _filepaths_config = json.load(json_file)
 
 
-_default_gene_disease_queries = [
+_gene_disease_queries = [
     ('ENSG00000101342', 'MONDO:0019557'),
     ('ENSG00000101347', 'MONDO:0015574'),
     ('ENSG00000213689', 'MONDO:0019557'),
     ('ENSG00000213689', 'MONDO:0018827'),
 ]
 
-_default_gene_disease_associations = (
-    GeneDiseaseAssociations
-    .from_filepath(_filepaths_config['gene_disease_associations'], spark)
+_gene_disease_associations_df = (
+    spark_read_csv(_filepaths_config['gene_disease_associations'], spark)
 )
 
 
-_default_disease_hierarchy = (
-    DiseaseHierarchy
-    .from_filepath(_filepaths_config['disease_hierarchy'], spark)
+_disease_hierarchy_df = (
+    spark_read_csv(_filepaths_config['disease_hierarchy'], spark)
 )
 
 
 def get_association_counts(
-    gene_disease_associations: GeneDiseaseAssociations = None,
-    disease_hierarchy: DiseaseHierarchy = None,
-    queries: List[tuple] = _default_gene_disease_queries,
+    gene_disease_associations_df: DataFrame = _gene_disease_associations_df,
+    disease_hierarchy_df: DataFrame = _disease_hierarchy_df,
+    queries: List[tuple] = _gene_disease_queries,
     spark: SparkSession = spark,
-):
-    if not gene_disease_associations:
-        gene_disease_associations = _default_gene_disease_associations
-    if not disease_hierarchy:
-        disease_hierarchy = _default_disease_hierarchy
+) -> DataFrame:
+    disease_hierarchy = DiseaseHierarchy(df=disease_hierarchy_df)
+    gene_disease_associations = GeneDiseaseAssociations(
+        df=gene_disease_associations_df,
+    )
     association_counter = AssociationCounter(
         disease_hierarchy=disease_hierarchy,
         gene_disease_associations=gene_disease_associations,
