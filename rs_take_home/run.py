22d754e41c6a31e56ffe41435b47a12a7d0e5f09
nicklamiller <nicklamiller@gmail.com>
2023-09-10 21:58:51 -0700

    make inputs strings

--- rs_take_home/run.py
+++ rs_take_home/run.py
@@ -1,23 +1,19 @@
-import json
 from typing import List
 
 from pyspark.sql import DataFrame, SparkSession
 
 from rs_take_home.association_counter import AssociationCounter
 from rs_take_home.data_models import DiseaseHierarchy, GeneDiseaseAssociations
-from rs_take_home.utils import spark_read_csv
-
-spark = (
-    SparkSession
-    .builder
-    .master('local[1]')
-    .appName('main')
-    .getOrCreate()
-)
 
 
-with open('config/filepaths.json') as json_file:
-    _filepaths_config = json.load(json_file)
+def _create_spark_session() -> SparkSession:
+    return (
+        SparkSession
+        .builder
+        .master('local[1]')
+        .appName('main')
+        .getOrCreate()
+    )
 
 
 _gene_disease_queries = [
@@ -27,26 +23,21 @@ _gene_disease_queries = [
     ('ENSG00000213689', 'MONDO:0018827'),
 ]
 
-_gene_disease_associations_df = (
-    spark_read_csv(_filepaths_config['gene_disease_associations'], spark)
-)
-
-
-_disease_hierarchy_df = (
-    spark_read_csv(_filepaths_config['disease_hierarchy'], spark)
-)
-
 
 def get_association_counts(
     *,
-    gene_disease_associations_df: DataFrame = _gene_disease_associations_df,
-    disease_hierarchy_df: DataFrame = _disease_hierarchy_df,
+    gene_disease_associations_path: str,
+    disease_hierarchy_path: str,
     queries: List[tuple] = _gene_disease_queries,
-    spark: SparkSession = spark,
+    spark: SparkSession = None,
 ) -> DataFrame:
-    disease_hierarchy = DiseaseHierarchy(df=disease_hierarchy_df)
-    gene_disease_associations = GeneDiseaseAssociations(
-        df=gene_disease_associations_df,
+    if not spark:
+        spark = _create_spark_session()
+    disease_hierarchy = DiseaseHierarchy.from_filepath(
+        disease_hierarchy_path, spark,
+    )
+    gene_disease_associations = GeneDiseaseAssociations.from_filepath(
+        gene_disease_associations_path, spark,
     )
     association_counter = AssociationCounter(
         disease_hierarchy=disease_hierarchy,
