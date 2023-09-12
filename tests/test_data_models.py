1342722c6f30c55edd9f0ee0783fd780c8c746d4
nicklamiller <nicklamiller@gmail.com>
2023-09-11 17:11:32 -0700

    parametrize tests for data model schema

--- tests/test_data_models.py
+++ tests/test_data_models.py
@@ -3,16 +3,26 @@ from pydantic import ValidationError
 from pyspark.sql import Row
 from pyspark_test import assert_pyspark_df_equal
 
-from rs_take_home.data_models import DiseaseHierarchy, GeneDiseaseAssociations
+from rs_take_home.data_models import GeneDiseaseAssociations
 from rs_take_home.utils import spark_read_csv
 
 
 @pytest.fixture
-def wrong_schema_df(spark_session):
+def wrong_columns_df(spark_session):
     return (
         spark_session.createDataFrame([
             Row(not_disease_id='made_up_id'),
             Row(not_disease_id='some_other_id'),
+        ])
+    )
+
+
+@pytest.fixture
+def wrong_dtypes_df(spark_session):
+    return (
+        spark_session.createDataFrame([
+            Row(gene_id=1),
+            Row(disease_id=True),
         ])
     )
 
@@ -29,11 +39,13 @@ def test_queries_df(queries, queries_df):
     )
 
 
+@pytest.mark.parametrize(
+    'wrong_schema_df',
+    [
+        (pytest.lazy_fixture('wrong_columns_df')),
+        (pytest.lazy_fixture('wrong_dtypes_df')),
+    ],
+)
 def test_gene_disease_associations_check_schema(wrong_schema_df):
     with pytest.raises(ValidationError):
         GeneDiseaseAssociations(df=wrong_schema_df)
-
-
-def test_disease_hierarchy_check_schema(wrong_schema_df):
-    with pytest.raises(ValidationError):
-        DiseaseHierarchy(df=wrong_schema_df)
